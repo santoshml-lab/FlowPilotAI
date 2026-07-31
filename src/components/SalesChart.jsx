fcb4e7ddc10ff3b3303eed367c7f3530defbb818
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-31 20:21:53 +0530

    Create SalesChart.jsx

---
+++ src/components/SalesChart.jsx
@@ -0,0 +1,30 @@
+export default function SalesChart() {
+  return (
+    <div
+      style={{
+        background: "#1e293b",
+        color: "white",
+        borderRadius: "15px",
+        padding: "25px",
+        marginTop: "30px",
+      }}
+    >
+      <h2>📈 Sales Analytics</h2>
+
+      <div
+        style={{
+          height: "300px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "#94a3b8",
+          border: "2px dashed #334155",
+          borderRadius: "10px",
+          marginTop: "20px",
+        }}
+      >
+        Sales Chart Coming Soon...
+      </div>
+    </div>
+  );
+}
