234968da7c36e0a6397e325275a9836bdda65212
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-01 02:02:26 +0530

    Update Input.jsx

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,1 +1,28 @@
-
+export default function Input({
+  type = "text",
+  placeholder,
+  value,
+  onChange,
+  name,
+}) {
+  return (
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      style={{
+        width: "100%",
+        padding: "12px 16px",
+        borderRadius: "12px",
+        border: "1px solid #334155",
+        background: "#1e293b",
+        color: "white",
+        outline: "none",
+        fontSize: "15px",
+        marginBottom: "15px",
+      }}
+    />
+  );
+}
