3a40cf36ba8eb24a32fd2318d363d6698a5a8ead
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-01 00:36:01 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,1 +1,14 @@
 
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_URL =
+"https://jpwjsaknniejzpokmire.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwd2pzYWtubmllanpwb2ttaXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzA2NjgsImV4cCI6MjA5ODMwNjY2OH0.0ur8EhnBmwZGNvBW_ql5pVzOy6OQ9pdDpds_BtYivMg";
+
+
+export const supabase = createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
