0c4327f68c721261fec9c94fa598d7cc22714b11
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-01 12:54:34 +0530

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,12 +4,12 @@ import Button from "../components/ui/Button";
 import Input from "../components/ui/Input";
 import Card from "../components/ui/Card";
 import Table from "../components/ui/Table";
-import { useNavigate } from "react-router-dom";
+
 export default function Products({ setPage }) {
 
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
-  const navigate = useNavigate();
+  
 
   useEffect(() => {
     loadProducts();
