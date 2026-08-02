936c73be7223dba1ac51447571e4e3e18c02df01
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-02 17:18:24 +0530

    Create Inventory.jsx

---
+++ src/pages/Inventory.jsx
@@ -0,0 +1,16 @@
+export default function Inventory() {
+  return (
+    <div
+      style={{
+        padding: "30px",
+        color: "white",
+      }}
+    >
+      <h1>📦 Inventory</h1>
+
+      <p>
+        Welcome to Inventory Module
+      </p>
+    </div>
+  );
+}
