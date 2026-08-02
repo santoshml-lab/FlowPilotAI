938d6a3d7047a7d155c6c59be2b77c627b318383
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-03 00:11:58 +0530

    Update Reports.jsx

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,6 +1,48 @@
 import Card from "../components/ui/Card";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabase";
+
 
 export default function Reports() {
+  const [revenue, setRevenue] = useState(0);
+const [sales, setSales] = useState(0);
+const [stock, setStock] = useState(0);
+const [customers, setCustomers] = useState(0);
+  useEffect(() => {
+  loadReports();
+}, []);
+
+async function loadReports() {
+  const { data: salesData } = await supabase
+    .from("sales")
+    .select("*");
+
+  const { data: productData } = await supabase
+    .from("products")
+    .select("*");
+
+  const { data: customerData } = await supabase
+    .from("customers")
+    .select("*");
+
+  setSales(salesData?.length || 0);
+
+  const totalRevenue = (salesData || []).reduce(
+    (sum, item) => sum + Number(item.total),
+    0
+  );
+
+  setRevenue(totalRevenue);
+
+  const totalStock = (productData || []).reduce(
+    (sum, item) => sum + Number(item.stock),
+    0
+  );
+
+  setStock(totalStock);
+
+  setCustomers(customerData?.length || 0);
+}
   return (
     <div
       style={{
@@ -22,22 +64,22 @@ export default function Reports() {
       >
         <Card>
           <h3>💰 Revenue Report</h3>
-          <h1>₹0</h1>
+          <h1>₹{revenue}</h1>
         </Card>
 
         <Card>
           <h3>🛒 Sales Report</h3>
-          <h1>0</h1>
+          <h1>{sales}</h1>
         </Card>
 
         <Card>
           <h3>📦 Stock Report</h3>
-          <h1>0</h1>
+          <h1>{stock}</h1>
         </Card>
 
         <Card>
           <h3>👥 Customers</h3>
-          <h1>0</h1>
+          <h1>{customers}</h1>
         </Card>
       </div>
     </div>
