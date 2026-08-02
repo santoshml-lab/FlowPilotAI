a805aa3c605e9855f12897d3450de1290ab4d68f
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-02 14:40:31 +0530

    Update ProductCharts.jsx

--- src/components/product/ProductCharts.jsx
+++ src/components/product/ProductCharts.jsx
@@ -22,7 +22,104 @@ export default function ProductCharts() {
             color: "#94a3b8",
           }}
         >
-          Chart Coming Soon...
+          <div
+  style={{
+    display: "flex",
+    flexDirection: "column",
+    gap: "15px",
+    marginTop: "20px",
+  }}
+>
+  <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <span>Electronics</span>
+      <span>75%</span>
+    </div>
+
+    <div
+      style={{
+        background: "#1e293b",
+        borderRadius: "10px",
+        overflow: "hidden",
+        height: "10px",
+        marginTop: "5px",
+      }}
+    >
+      <div
+        style={{
+          width: "75%",
+          background: "#3b82f6",
+          height: "100%",
+        }}
+      />
+    </div>
+  </div>
+
+  <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <span>Fashion</span>
+      <span>45%</span>
+    </div>
+
+    <div
+      style={{
+        background: "#1e293b",
+        borderRadius: "10px",
+        overflow: "hidden",
+        height: "10px",
+        marginTop: "5px",
+      }}
+    >
+      <div
+        style={{
+          width: "45%",
+          background: "#22c55e",
+          height: "100%",
+        }}
+      />
+    </div>
+  </div>
+
+  <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <span>Accessories</span>
+      <span>25%</span>
+    </div>
+
+    <div
+      style={{
+        background: "#1e293b",
+        borderRadius: "10px",
+        overflow: "hidden",
+        height: "10px",
+        marginTop: "5px",
+      }}
+    >
+      <div
+        style={{
+          width: "25%",
+          background: "#f59e0b",
+          height: "100%",
+        }}
+      />
+    </div>
+  </div>
+</div>
         </div>
       </Card>
 
