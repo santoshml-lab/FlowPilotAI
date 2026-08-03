8b42a7a5bfc08fe3e6e04b1cc1b71b035858f5c6
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-03 20:57:45 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
+const API = "https://salespilot-l1d3.onrender.com";
 
+export async function getDashboard() {
+
+  const response = await fetch(`${API}/dashboard`);
+
+  return await response.json();
+
+}
