3666f085de3f89363ad970e4951a30cf6dcc98e7
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-06 02:26:24 +0530

    Update TopProductsChart.jsx

--- src/components/dashboard/TopProductsChart.jsx
+++ src/components/dashboard/TopProductsChart.jsx
@@ -1,30 +1,57 @@
+import { useEffect, useState } from "react";
+import { getTopProducts } from "../../services/api";
+
 import {
+  ResponsiveContainer,
   BarChart,
   Bar,
   XAxis,
   YAxis,
   Tooltip,
-  ResponsiveContainer,
+  CartesianGrid,
 } from "recharts";
 
-const data = [
-  { product: "Laptop", sold: 15 },
-  { product: "Mouse", sold: 32 },
-  { product: "Keyboard", sold: 20 },
-  { product: "Monitor", sold: 8 },
-];
-
 export default function TopProductsChart() {
-  return (
-    <div className="card">
-      <h2>🏆 Top Selling Products</h2>
 
-      <ResponsiveContainer width="100%" height={300}>
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    loadChart();
+  }, []);
+
+  async function loadChart() {
+    const result = await getTopProducts();
+    setData(result);
+  }
+
+  return (
+    <div
+      style={{
+        background: "#1e293b",
+        padding: "20px",
+        borderRadius: "15px",
+        marginTop: "25px",
+      }}
+    >
+      <h2 style={{ color: "white" }}>
+        🏆 Top Selling Products
+      </h2>
+
+      <ResponsiveContainer width="100%" height={350}>
         <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+
           <XAxis dataKey="product" />
+
           <YAxis />
+
           <Tooltip />
-          <Bar dataKey="sold" fill="#22c55e" />
+
+          <Bar
+            dataKey="sold"
+            fill="#3b82f6"
+            radius={[8, 8, 0, 0]}
+          />
         </BarChart>
       </ResponsiveContainer>
     </div>
