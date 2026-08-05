bb81547fc26c39974b1fb346ad390d44cb528727
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-05 19:01:36 +0530

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -59,9 +59,9 @@ export default function Sidebar({ setPage }) {
 >
   📜 Inventory History
 </div>
-      <button onClick={() => setPage("notifications")}>
-  🔔 Notifications
-</button>
+      
+  
+
         
       
 
