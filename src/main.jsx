af6326679573d3722060a861230832d51b21c151
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-06 11:55:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,9 @@ import "./styles/theme.css";
 import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  <ThemeProvider>
+  
+    
+   <ThemeProvider>
     <App />
 </ThemeProvider>
 );
