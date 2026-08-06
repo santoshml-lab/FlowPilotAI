28b29276ddd051648bfbabc6d5181ebd7fea00d3
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-07 00:40:04 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { supabase } from "../lib/supabase";
+import { useNavigate } from "react-router-dom";
 
 export default function Login({ onLogin, goSignup }) {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   async function login() {
   
