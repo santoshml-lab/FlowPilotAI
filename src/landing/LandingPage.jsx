ca02c46d7565a88de6874ed788908d79cef0d8a0
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-07 00:04:53 +0530

    Create LandingPage.jsx

---
+++ src/landing/LandingPage.jsx
@@ -0,0 +1,13 @@
+import LandingNavbar from "./Navbar";
+import Hero from "./Hero";
+import Features from "./Features";
+
+export default function LandingPage() {
+  return (
+    <>
+      <LandingNavbar />
+      <Hero />
+      <Features />
+    </>
+  );
+}
