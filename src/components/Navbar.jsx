830a91dece729fd865c4c47bc6fe3c0aa206b411
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-07 22:00:48 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { supabase } from "../lib/supabase";
+const [profileOpen, setProfileOpen] = useState(false);
 
 
 export default function Navbar() {
